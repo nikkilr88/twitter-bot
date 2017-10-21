5c7859e97b340e57e0ee1ed06e5b15ffaaffce8a
Nicole Sattler <nicole.sattler@live.com>
2017-10-21 19:36:29 +0000

    Modified cofig variables

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var Twit = require("twit"),
     hashtags = require("./helper-files/hashtags");
 
 var Bot = new Twit({
-    consumer_key: consumer_key,
-    consumer_secret: consumer_secret,
-    access_token: access_token,
-    access_token_secret: access_token_secret
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token: process.env.access_token,
+    access_token_secret: process.env.access_token_secret
 });
 
 var tweetStream = Bot.stream('statuses/filter', { track: hashtags });
