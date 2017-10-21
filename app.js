4ace2fe6ea244e28fe385097c8fc805691ef6264
Nicole Sattler <nicole.sattler@live.com>
2017-10-21 22:38:10 +0000

    Added if statement to followed function

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var followStream = Bot.stream('user');
 followStream.on('follow', followed);
 
 tweetStream.on('tweet', function(tweet) {
-    if(!isReply(tweet)){
+    if (!isReply(tweet)) {
         var tweetId = tweet.id_str;
         retweet(tweetId);
     }
@@ -23,7 +23,7 @@ tweetStream.on('tweet', function(tweet) {
 
 //Retweet tweets of followed hastags
 function retweet(tweetId) {
-    
+
     Bot.post('statuses/retweet/:id', { id: tweetId }, function(err, data, response) {
         if (err) {
             console.log("Something went wrong... :(");
@@ -35,28 +35,31 @@ function retweet(tweetId) {
 }
 
 //Follow users back
-function followed(event){
+function followed(event) {
     var userId = event.source.id_str;
     var screenName = event.source.screen_name;
     console.log(event)
-    
-    Bot.post('friendships/create', {screen_name: screenName}, function(err, data, response){
-        if(err) {
-            console.log("Something went wrong... :(")
-            console.log(err);
-        } else {
-            console.log("Followed back!");
-        }
-    });
+
+    if (userId !== "gitLit000") {
+        Bot.post('friendships/create', { screen_name: screenName }, function(err, data, response) {
+            if (err) {
+                console.log("Something went wrong... :(")
+                console.log(err);
+            }
+            else {
+                console.log("Followed back!");
+            }
+        });
+    }
 }
 
 //Check if tweet is retweet
 function isReply(tweet) {
-  if ( tweet.retweeted_status
-    || tweet.in_reply_to_status_id
-    || tweet.in_reply_to_status_id_str
-    || tweet.in_reply_to_user_id
-    || tweet.in_reply_to_user_id_str
-    || tweet.in_reply_to_screen_name )
-    return true
+    if (tweet.retweeted_status ||
+        tweet.in_reply_to_status_id ||
+        tweet.in_reply_to_status_id_str ||
+        tweet.in_reply_to_user_id ||
+        tweet.in_reply_to_user_id_str ||
+        tweet.in_reply_to_screen_name)
+        return true
 }
