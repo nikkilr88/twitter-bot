2f7f4e45e066a1aa7b0bcc8e2c9e4da1c16194aa
Nicole Sattler <nicole.sattler@live.com>
2017-10-23 16:29:42 +0000

    Commented out function to follow back, started tracking users

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var Twit = require("twit"),
-    hashtags = require("./helper-files/hashtags");
+    tracking = require("./helper-files/track");
 
+//Bot config
 var Bot = new Twit({
     consumer_key: process.env.consumer_key,
     consumer_secret: process.env.consumer_secret,
@@ -8,18 +9,21 @@ var Bot = new Twit({
     access_token_secret: process.env.access_token_secret
 });
 
-var tweetStream = Bot.stream('statuses/filter', { track: hashtags });
-var followStream = Bot.stream('user');
+//Stream config to follow users and hashtags
+var tweetStream = Bot.stream('statuses/filter', { track: tracking.hashtags, follow: tracking.users });
+// var followStream = Bot.stream('user');
 
-followStream.on('follow', followed);
+//Listen for follow events
+// followStream.on('follow', followed);
 
+//Listen for tweet events from tracked hashtags and users
 tweetStream.on('tweet', function(tweet) {
     if (!isReply(tweet)) {
         var tweetId = tweet.id_str;
+        console.log(tweet.text);
         retweet(tweetId);
     }
 });
-
 
 //Retweet tweets of followed hastags
 function retweet(tweetId) {
@@ -27,6 +31,7 @@ function retweet(tweetId) {
     Bot.post('statuses/retweet/:id', { id: tweetId }, function(err, data, response) {
         if (err) {
             console.log("Something went wrong... :(");
+            console.log(err);
         }
         else {
             console.log("Retweeted! :)");
@@ -39,9 +44,7 @@ function followed(event) {
 
     var screenName = event.source.screen_name;
 
-    console.log(event);
-
-    if (screenName !== "gitLit000") {
+    if (screenName !== "GitLit000") {
         Bot.post('friendships/create', { screen_name: screenName }, function(err, data, response) {
             if (err) {
                 console.log("Something went wrong... :(")
