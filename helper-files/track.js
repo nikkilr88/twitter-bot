8f5aafabe59932da5e54f33cf7b4a3a40e9a306f
Nicole Sattler <nicole.sattler@live.com>
2017-10-23 17:25:47 +0000

    Added user id to users array

--- helper-files/track.js
+++ helper-files/track.js
@@ -1,6 +1,6 @@
 module.exports = { 
     hashtags: ['#100daysofcode', '#100DaysOfCode', '#codenewbie', '#CodeNewbie', '#301daysofcode', '#301DaysOfCode', '#freecodecamp', '#FreeCodeCamp', "#freeCodeCamp"],
-    users: [819717383975739392, 870038005, 3152347960, 1960930698, 2398551432, 88050326]
+    users: [819717383975739392, 870038005, 3152347960, 1960930698, 2398551432, 1668100142, 88050326]
 };
 
-// thecodingtrain, traversymedia, thenetninjauk, PizzaPokerGuy, CodeNewbies, Me
+// thecodingtrain, traversymedia, thenetninjauk, PizzaPokerGuy, CodeNewbies, freeCodeCamp, Me
