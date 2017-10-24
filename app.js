cce973befc41a4bdb19b0b9b5e294753764fbd56
Nicole Sattler <nicole.sattler@live.com>
2017-10-24 22:58:28 +0000

    Refactored and added coin flip function to limit 100daysofcode tweets

--- app.js
+++ app.js
@@ -1,65 +1,63 @@
 var Twit = require("twit"),
+    keys = require("./helper-files/keys"),
     tracking = require("./helper-files/track");
 
 //Bot config
 var Bot = new Twit({
-    consumer_key: process.env.consumer_key,
-    consumer_secret: process.env.consumer_secret,
-    access_token: process.env.access_token,
-    access_token_secret: process.env.access_token_secret
+    consumer_key: /*keys.consumer_key,*/ process.env.consumer_key,
+    consumer_secret: /*keys.consumer_secret,*/ process.env.consumer_secret,
+    access_token: /*keys.access_token,*/ process.env.access_token,
+    access_token_secret: /*keys.access_token_secret*/ process.env.access_token_secret
 });
 
-//Stream config to follow users and hashtags
-var tweetStream = Bot.stream('statuses/filter', { track: tracking.hashtags, follow: tracking.users });
-// var followStream = Bot.stream('user');
+//Tweet stream config to follow users and hashtags
+var stream = Bot.stream('statuses/filter', { track: tracking.hashtags, follow: tracking.users });
 
-//Listen for follow events
-// followStream.on('follow', followed);
+//#100DaysOfCode tweeet stream config
+var DOCstream = Bot.stream('statuses/filter', { track: ['#100DaysOfCode', '100daysofcode'] });
 
 //Listen for tweet events from tracked hashtags and users
-tweetStream.on('tweet', function(tweet) {
-    if (!isReply(tweet)) {
+stream.on('tweet', function(tweet) {
+    
+    //Retweet if tweet is not a reply
+    if (!isReply(tweet) && tweet.user.id_str !== '872974956249960448') {
         var tweetId = tweet.id_str;
-        console.log(tweet.text);
         retweet(tweetId);
     }
 });
 
-//Retweet tweets of followed hastags
+//Listen for #100days of code tweet events
+DOCstream.on('tweet', function(tweet) {
+    
+    //Check if tweet begins with RT
+    var RT = new RegExp("^RT", "i");
+    
+    console.log("100days: ", tweet.text);
+    
+    //Retweet if tweet is not reply, doesn't start with RT and coinFlip returns 0
+    if (!isReply(tweet) && !tweet.text.match(RT) && coinFlip() === 0){
+        var tweetId = tweet.id_str;
+        retweet(tweetId);
+    }
+});
+
+//Retweet tweets of followed hastags and users
 function retweet(tweetId) {
 
     Bot.post('statuses/retweet/:id', { id: tweetId }, function(err, data, response) {
         if (err) {
-            console.log("Something went wrong... :(");
             console.log(err);
         }
         else {
-            console.log("Retweeted! :)");
+            console.log("Retweeted! :)", data.text);
         }
     });
 }
 
-//Follow users back
-function followed(event) {
-
-    var screenName = event.source.screen_name;
-
-    if (screenName !== "GitLit000") {
-        Bot.post('friendships/create', { screen_name: screenName }, function(err, data, response) {
-            if (err) {
-                console.log("Something went wrong... :(")
-                console.log(err);
-            }
-            else {
-                console.log("Followed back!");
-            }
-        });
-    }
-}
-
-//Check if tweet is retweet
+//Check if tweet is retweet or reply
 function isReply(tweet) {
-    if (tweet.retweeted_status ||
+    if (tweet.is_quote_status ||
+        tweet.retweeted_status ||
         tweet.in_reply_to_status_id ||
         tweet.in_reply_to_status_id_str ||
         tweet.in_reply_to_user_id ||
@@ -67,3 +65,9 @@ function isReply(tweet) {
         tweet.in_reply_to_screen_name)
         return true
 }
+
+//Function to limit #100daysofcode retweets
+function coinFlip(){
+  return Math.floor(Math.random() * 2)
+}
+
