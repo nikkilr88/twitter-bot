e454419f065617fb4efb958851019d341b84be37
Nicole Sattler <nicole.sattler@live.com>
2017-11-01 17:17:30 +0000

    Added block array

--- helper-files/track.js
+++ helper-files/track.js
@@ -1,9 +1,12 @@
 module.exports = { 
     hashtags: ['#codenewbie', '#301daysofcode', '#freecodecamp'],
     users: ['819717383975739392', '870038005', '3152347960', '1960930698', '2398551432', '1668100142', '341643950', '88050326'],
-    ytChannels: ['UC8butISFwT-Wl7EV0hUK0BQ', 'UCW5YeuERMmlnqo4oq8vwUpg', 'UCvjgXvBlbQiydffZU7m1_aw', 'UC29ju8bIPH5as8OGnQzwJyA']
+    block:  ['872974956249960448', '842956176958476289'],
+    ytChannels: ['UC8butISFwT-Wl7EV0hUK0BQ', 'UCW5YeuERMmlnqo4oq8vwUpg', 'UCvjgXvBlbQiydffZU7m1_aw', 'UC29ju8bIPH5as8OGnQzwJyA'],
 };
 
 //users: thecodingtrain, traversymedia, thenetninjauk, PizzaPokerGuy, CodeNewbies, freeCodeCamp, ossia, Me 
+
+//block: fccbarkinet, _30days30sites
 
 //ytChannels = freeCodeCamp, The Net Ninja, The Coding Train, Traversy Media
