f9154acd8a21565316e9272594bd4fdcc0563557
Nicole Sattler <nicole.sattler@live.com>
2017-11-01 16:47:55 +0000

    Required post-vid helper function for testing

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var Twit = require('twit'),
     tracking = require('./helper-files/track'),
     blocked = require('./helper-files/block'),
     Bot = require('./config/bot'),
-    functions = require('./helper-files/tweet-functions');
+    functions = require('./helper-files/tweet-functions'),
+    tweetVid = require('./helper-files/post-vid');
 
 var baseURL = 'https://www.youtube.com/watch?v=',
     RT = /^RT/i;
+    
+tweetVid();
 
 //Tweet stream config to follow users and hashtags
 var stream = Bot.stream('statuses/filter', { track: tracking.hashtags, follow: tracking.users });
@@ -25,30 +28,30 @@ stream.on('tweet', function(tweet) {
     }
 });
 
-setInterval(function() {
+// setInterval(function() {
 
-    //Get video data from random YT channels in array
-    var url = 'https://www.googleapis.com/youtube/v3/search?key=' + /*keys.yt*/ process.env.yt_key + '&channelId=' + tracking.ytChannels[rand(4)] + '&part=snippet,id&order=date&maxResults=50';
+//     //Get video data from random YT channels in array
+//     var url = 'https://www.googleapis.com/youtube/v3/search?key=' + /*keys.yt*/ process.env.yt_key + '&channelId=' + tracking.ytChannels[rand(4)] + '&part=snippet,id&order=date&maxResults=50';
     
-    request(url, function(err, res, body) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            var data = JSON.parse(body),
-                randVid = rand(50),
-                title = data.items[randVid].snippet.title,
-                vidId = data.items[randVid].id.videoId,
-                status = title + " " + baseURL + vidId;
+//     request(url, function(err, res, body) {
+//         if (err) {
+//             console.log(err);
+//         }
+//         else {
+//             var data = JSON.parse(body),
+//                 randVid = rand(50),
+//                 title = data.items[randVid].snippet.title,
+//                 vidId = data.items[randVid].id.videoId,
+//                 status = title + " " + baseURL + vidId;
 
-            //If there is a video id, pass it to the function
-            if (vidId) {
+//             //If there is a video id, pass it to the function
+//             if (vidId) {
 
-                functions.postVid(vidId, status);
-            }
-        }
-    });
-}, 1000 * 60 * 60 * 6);
+//                 functions.postVid(vidId, status);
+//             }
+//         }
+//     });
+// }, 1000 * 60 * 60 * 6);
 
 //Get random number
 function rand(max) {
