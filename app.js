b85f31311951a5486de948d025f66d1eef1b0446
Nicole Sattler <nicole.sattler@live.com>
2017-11-01 17:20:03 +0000

    Refatored code

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-var Twit = require('twit'),
-    request = require('request'),
-    // keys = require(''./helper-files/keys'),
+var Bot = require('./config/bot'),
     tracking = require('./helper-files/track'),
-    blocked = require('./helper-files/block'),
-    Bot = require('./config/bot'),
     functions = require('./helper-files/tweet-functions'),
-    tweetVid = require('./helper-files/post-vid');
+    tweetVid = require('./helper-files/tweet-vid');
 
-var baseURL = 'https://www.youtube.com/watch?v=',
-    RT = /^RT/i;
-    
+//Post random YT coding video
 setInterval(tweetVid, 1000 * 60 * 60 * 6);
 
 //Tweet stream config to follow users and hashtags
@@ -21,39 +15,9 @@ stream.on('tweet', function(tweet) {
     console.log(tweet.text);
 
     //Retweet if tweet is not a reply and not that one annoying user
-    if (!functions.isReply(tweet) && !blocked.includes(tweet.user.id_str)) {
+    if (!functions.isReply(tweet) && !tracking.blocked.includes(tweet.user.id_str)) {
         
         console.log('New tweet: ' + tweet.text + '\n');
         functions.retweet(tweet.id_str);
     }
 });
-
-// setInterval(function() {
-
-//     //Get video data from random YT channels in array
-//     var url = 'https://www.googleapis.com/youtube/v3/search?key=' + /*keys.yt*/ process.env.yt_key + '&channelId=' + tracking.ytChannels[rand(4)] + '&part=snippet,id&order=date&maxResults=50';
-    
-//     request(url, function(err, res, body) {
-//         if (err) {
-//             console.log(err);
-//         }
-//         else {
-//             var data = JSON.parse(body),
-//                 randVid = rand(50),
-//                 title = data.items[randVid].snippet.title,
-//                 vidId = data.items[randVid].id.videoId,
-//                 status = title + " " + baseURL + vidId;
-
-//             //If there is a video id, pass it to the function
-//             if (vidId) {
-
-//                 functions.postVid(vidId, status);
-//             }
-//         }
-//     });
-// }, 1000 * 60 * 60 * 6);
-
-//Get random number
-function rand(max) {
-    return Math.floor(Math.random() * max);
-}
