4ca7caa20978f8fc27a26102a1f6c7e5dd5bec83
Nicole Sattler <nicole.sattler@live.com>
2018-01-31 22:07:03 +0000

    Modify tweet stream function logic to filter out tweets with 5 or more hashtags

--- app.js
+++ app.js
@@ -11,12 +11,25 @@ var stream = Bot.stream('statuses/filter', { track: tracking.hashtags, follow: t
 
 //Listen for tweet events from tracked hashtags and users
 stream.on('tweet', function(tweet) {
-    
-    console.log('TEXT: ', tweet.text);
-    console.log('HASHTAGS', tweet.entities.hashtags.length);
-
+    console.log(tweet.text);
     //Retweet if tweet is not a reply and not that one annoying user
-    if (!functions.isReply(tweet) && tracking.block.indexOf(tweet.user.id_str) == -1 && tweet.entities.hashtags.length <= 4) {
-        functions.retweet(tweet.id_str);
+    if (!functions.isReply(tweet) && tracking.block.indexOf(tweet.user.id_str) == -1) {
+        
+        console.log(tweet);
+        
+        if(tweet.extended_tweet != null && tweet.extended_tweet.entities.hashtags.length <= 4) {
+            console.log('LENGTH EXTENDED: ', tweet.extended_tweet.entities.hashtags.length);
+            functions.retweet(tweet.id_str);
+        }
+        
+        if(tweet.retweeted_status != null && tweet.retweeted_status.extended_tweet.entities.hashtags.length <= 4) {
+            console.log('LENGTH EXTENDED: ', tweet.retweeted_status.extended_tweet.entities.hashtags.length);
+            functions.retweet(tweet.id_str);
+        }
+        
+        if(!tweet.truncated && tweet.entities.hashtags.length <= 4) {
+            console.log('LENGTH: ', tweet.entities.hashtags.length);
+            functions.retweet(tweet.id_str);
+        }
     }
 });
