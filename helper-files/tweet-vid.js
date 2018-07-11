6b8ada8948a37ac2c9fa9361d0b8f6a13d911d91
Nicole <nicole.sattler@live.com>
2018-07-11 11:27:42 -0500

    Refactor

--- helper-files/tweet-vid.js
+++ helper-files/tweet-vid.js
@@ -1,11 +1,9 @@
-var request = require('request'),
-    tracking = require('./track'),
-    functions = require('./tweet-functions'),
-    baseURL = 'https://www.youtube.com/watch?v=';
+const request = require('request'),
+      {  ytChannels } = require('./track'),
+      { postVid } = require('./tweet-functions'),
+      baseURL = 'https://www.youtube.com/watch?v=';
     
-// var keys = require('../config/keys');
-
-var url = 'https://www.googleapis.com/youtube/v3/search?key=' + /*keys.yt*/ process.env.yt_key + '&channelId=' + tracking.ytChannels[rand(4)] + '&part=snippet,id&order=date&maxResults=50';
+let url = 'https://www.googleapis.com/youtube/v3/search?key=' + process.env.yt_key + '&channelId=' + ytChannels[rand(4)] + '&part=snippet,id&order=date&maxResults=50';
 
 //Get random number
 function rand(max) {
@@ -14,21 +12,17 @@ function rand(max) {
 
 module.exports = function() {
     request(url, function(err, res, body) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            var data = JSON.parse(body),
-                randVid = rand(50),
-                title = data.items[randVid].snippet.title,
-                vidId = data.items[randVid].id.videoId,
-                status = title + " " + baseURL + vidId;
+        if(err) return console.log(err);
+        
+        let data = JSON.parse(body),
+            randVid = rand(50),
+            title = data.items[randVid].snippet.title,
+            vidId = data.items[randVid].id.videoId,
+            status = title + " " + baseURL + vidId + ' ' + '#100DaysOfCode';
 
-            //If there is a video id, pass it to the function
-            if (vidId) {
-                functions.postVid(vidId, status);
-            }
+        //If there is a video id, pass it to the function
+        if (vidId) {
+            postVid(vidId, status);
         }
     });
-
 };
