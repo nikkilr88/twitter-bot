8156f6e82cda5f30e0a182518fd7b6da69532f67
Nicole <nicole.sattler@live.com>
2018-07-11 11:27:04 -0500

    Remove comments

--- config/bot.js
+++ config/bot.js
@@ -1,9 +1,8 @@
 var Twit = require("twit");
-// var keys = require('./keys');
 
 module.exports = new Twit({
-    consumer_key: /*keys.consumer_key,*/ process.env.consumer_key,
-    consumer_secret: /*keys.consumer_secret,*/ process.env.consumer_secret,
-    access_token: /*keys.access_token,*/ process.env.access_token,
-    access_token_secret: /*keys.access_token_secret*/ process.env.access_token_secret
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token: process.env.access_token,
+    access_token_secret: process.env.access_token_secret
 });
