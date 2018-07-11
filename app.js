e2ee7f296095ec0fec0a067e9cbc74638341e11a
Nicole <nicole.sattler@live.com>
2018-07-11 11:26:52 -0500

    Refactor code to skip spam tweets, update console.logs

--- app.js
+++ app.js
@@ -1,35 +1,45 @@
-var Bot = require('./config/bot'),
-    tracking = require('./helper-files/track'),
-    functions = require('./helper-files/tweet-functions'),
-    tweetVid = require('./helper-files/tweet-vid');
+require('dotenv').config();
 
-//Post random YT coding video
+const Bot = require('./config/bot'),
+      { hashtags, users, block } = require('./helper-files/track'),
+      { retweet, isReply } = require('./helper-files/tweet-functions'),
+      tweetVid = require('./helper-files/tweet-vid');
+
+//Post random YT coding video every 6 hours
 setInterval(tweetVid, 1000 * 60 * 60 * 6);
 
 //Tweet stream config to follow users and hashtags
-var stream = Bot.stream('statuses/filter', { track: tracking.hashtags, follow: tracking.users });
+var stream = Bot.stream('statuses/filter', { track: hashtags, follow: users });
 
 //Listen for tweet events from tracked hashtags and users
 stream.on('tweet', function(tweet) {
-    console.log(tweet.text);
+    console.log('INCOMING: ', tweet.text);
     //Retweet if tweet is not a reply and not that one annoying user
-    if (!functions.isReply(tweet) && tracking.block.indexOf(tweet.user.id_str) == -1) {
+    if (!isReply(tweet) && block.indexOf(tweet.user.id_str) == -1) {
         
-        console.log(tweet);
+        console.log('!ISREPLY: ', tweet);
+        let extended = tweet.extended_tweet,
+            retweetedExtended = tweet.retweeted_status;
         
-        if(tweet.extended_tweet != null && tweet.extended_tweet.entities.hashtags.length <= 4) {
-            console.log('LENGTH EXTENDED: ', tweet.extended_tweet.entities.hashtags.length);
-            functions.retweet(tweet.id_str);
-        }
+        // Skip over tweets with too may hashtags
+        // if(extended != null && extended.entities.hashtags.length <= 4) {
+        //     console.log('LENGTH EXTENDED: ', extended.entities.hashtags.length);
+        //     retweet(tweet.id_str);
+        // }
         
-        if(tweet.retweeted_status != null && tweet.retweeted_status.extended_tweet.entities.hashtags.length <= 4) {
-            console.log('LENGTH EXTENDED: ', tweet.retweeted_status.extended_tweet.entities.hashtags.length);
-            functions.retweet(tweet.id_str);
-        }
+        // if(retweetedExtended != null && retweetedExtended.extended_tweet.entities.hashtags.length <= 4) {
+        //     console.log('LENGTH EXTENDED: ', retweetedExtended.extended_tweet.entities.hashtags.length);
+        //     retweet(tweet.id_str);
+        // }
         
         if(!tweet.truncated && tweet.entities.hashtags.length <= 4) {
             console.log('LENGTH: ', tweet.entities.hashtags.length);
-            functions.retweet(tweet.id_str);
+            retweet(tweet.id_str);
+        } else if (extended != null && extended.entities.hashtags.length <= 4){
+            console.log('LENGTH EXTENDED: ', extended.entities.hashtags.length);
+            retweet(tweet.id_str);
+        } else {
+            return;
         }
     }
 });
