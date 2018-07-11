4bd82eef8a222af0975c025a64bdec9d2f530bb2
Nicole <nicole.sattler@live.com>
2018-07-11 11:27:25 -0500

    Refactor

--- helper-files/tweet-functions.js
+++ helper-files/tweet-functions.js
@@ -1,24 +1,17 @@
 var Bot = require('../config/bot');
 
-var functions = {};
-
 //Retweet tweets of followed hastags and users
-functions.retweet = function(tweetId) {
+exports.retweet = function(tweetId) {
     Bot.post('statuses/retweet/:id', { id: tweetId }, function(err, data, response) {
-        if (err) {
-            console.log(tweetId);
-            console.log(err.message);
-        }
-        else {
-            console.log('Retweeted! :)' + data.text + '\n');
-        }
+        if(err) return console.log(tweetId, err.message);
+        console.log('Retweeted! :)' + data.text + '\n');
     });
 };
 
 //Check if tweet is retweet or reply
-var RT = /^RT/i;
+const RT = /^RT/i;
 
-functions.isReply =  function(tweet) {
+exports.isReply =  function(tweet) {
     if (RT.test(tweet.text) ||
         tweet.is_quote_status ||
         tweet.retweeted_status ||
@@ -31,18 +24,10 @@ functions.isReply =  function(tweet) {
 };
 
 //New tutorial post
-functions.postVid = function(id, status) {
-
+exports.postVid = function(id, status) {
     console.log(status);
-
     Bot.post('statuses/update', { status: status }, function(err, data, response) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            console.log("Sucessfully posted video! :)");
-        }
+        if(err) return console.log(err);
+        console.log('Sucessfully posted video! :)');
     });
 };
-
-module.exports = functions;
