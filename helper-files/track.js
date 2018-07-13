47b975b7a2cbc76e01ed33446a74bf0ec6c55b37
Nicole <nicole.sattler@live.com>
2018-07-13 08:52:27 -0500

    Add YT channel

--- helper-files/track.js
+++ helper-files/track.js
@@ -2,11 +2,11 @@ module.exports = {
     hashtags: ['#codenewbie', '#301daysofcode', '#freecodecamp'],
     users: ['819717383975739392', '870038005', '3152347960', '1960930698', '2398551432', '1668100142', '341643950', '88050326'],
     block:  ['872974956249960448', '842956176958476289', '744776302591303680'],
-    ytChannels: ['UC8butISFwT-Wl7EV0hUK0BQ', 'UCW5YeuERMmlnqo4oq8vwUpg', 'UCvjgXvBlbQiydffZU7m1_aw', 'UC29ju8bIPH5as8OGnQzwJyA'],
+    ytChannels: ['UC8butISFwT-Wl7EV0hUK0BQ', 'UCW5YeuERMmlnqo4oq8vwUpg', 'UCvjgXvBlbQiydffZU7m1_aw', 'UC29ju8bIPH5as8OGnQzwJyA', 'UCyU5wkjgQYGRB0hIHMwm2Sg'],
 };
 
 //users: thecodingtrain, traversymedia, thenetninjauk, PizzaPokerGuy, CodeNewbies, freeCodeCamp, ossia, Me 
 
 //block: fccbarkinet, _30days30sites
 
-//ytChannels = freeCodeCamp, The Net Ninja, The Coding Train, Traversy Media
+//ytChannels = freeCodeCamp, The Net Ninja, The Coding Train, Traversy Media, LevelUp Tuts
