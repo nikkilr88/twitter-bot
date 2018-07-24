49a4fef61accd241ca6df0d00ef5016da209c212
Nicole <nicole.sattler@live.com>
2018-07-24 18:14:00 -0500

    Add YouTube Channel

--- helpers/track.js
+++ helpers/track.js
@@ -2,11 +2,11 @@ module.exports = {
     hashtags: ['#codenewbie', '#301daysofcode', '#freecodecamp'],
     users: ['819717383975739392', '870038005', '3152347960', '1960930698', '2398551432', '1668100142', '341643950'],
     block: ['872974956249960448', '842956176958476289', '744776302591303680', '921765201191596032'],
-    ytChannels: ['UC8butISFwT-Wl7EV0hUK0BQ', 'UCW5YeuERMmlnqo4oq8vwUpg', 'UCvjgXvBlbQiydffZU7m1_aw', 'UC29ju8bIPH5as8OGnQzwJyA', 'UCyU5wkjgQYGRB0hIHMwm2Sg', 'UCxX9wt5FWQUAAz4UrysqK9A', 'UC5Wi_NYysX-LfcqT3Hq9Faw'],
+    ytChannels: ['UC8butISFwT-Wl7EV0hUK0BQ', 'UCW5YeuERMmlnqo4oq8vwUpg', 'UCvjgXvBlbQiydffZU7m1_aw', 'UC29ju8bIPH5as8OGnQzwJyA', 'UCyU5wkjgQYGRB0hIHMwm2Sg', 'UCxX9wt5FWQUAAz4UrysqK9A', 'UC5Wi_NYysX-LfcqT3Hq9Faw', 'UCVyRiMvfUNMA1UPlDPzG5Ow'],
 };
 
 //users: thecodingtrain, traversymedia, thenetninjauk, PizzaPokerGuy, CodeNewbies, freeCodeCamp, ossia
 
 //block: fccbarkinet, _30days30sites, this bot
 
-//ytChannels = freeCodeCamp, The Net Ninja, The Coding Train, Traversy Media, LevelUp Tuts, CS Dojo, Coding tutorials 360
+//ytChannels = freeCodeCamp, The Net Ninja, The Coding Train, Traversy Media, LevelUp Tuts, CS Dojo, Coding tutorials 360, Design Course
