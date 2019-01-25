1bdf5479e14efd12a1ebde3f3b0cec0343ce328a
Nicole <nicole.sattler@live.com>
2019-01-25 11:56:37 -0600

    Add myself to users

--- helpers/track.js
+++ helpers/track.js
@@ -1,11 +1,34 @@
-module.exports = { 
-    hashtags: ['#codenewbie', '#301daysofcode', '#freecodecamp'],
-    users: ['819717383975739392', '870038005', '3152347960', '1960930698', '2398551432', '1668100142', '341643950'],
-    block: ['872974956249960448', '842956176958476289', '744776302591303680', '921765201191596032'],
-    ytChannels: ['UC8butISFwT-Wl7EV0hUK0BQ', 'UCW5YeuERMmlnqo4oq8vwUpg', 'UCvjgXvBlbQiydffZU7m1_aw', 'UC29ju8bIPH5as8OGnQzwJyA', 'UCyU5wkjgQYGRB0hIHMwm2Sg', 'UCxX9wt5FWQUAAz4UrysqK9A', 'UC5Wi_NYysX-LfcqT3Hq9Faw', 'UCVyRiMvfUNMA1UPlDPzG5Ow'],
-};
+module.exports = {
+  hashtags: ['#codenewbie', '#301daysofcode', '#freecodecamp'],
+  users: [
+    '819717383975739392',
+    '870038005',
+    '3152347960',
+    '1960930698',
+    '2398551432',
+    '1668100142',
+    '341643950',
+    '88050326'
+  ],
+  block: [
+    '872974956249960448',
+    '842956176958476289',
+    '744776302591303680',
+    '921765201191596032'
+  ],
+  ytChannels: [
+    'UC8butISFwT-Wl7EV0hUK0BQ',
+    'UCW5YeuERMmlnqo4oq8vwUpg',
+    'UCvjgXvBlbQiydffZU7m1_aw',
+    'UC29ju8bIPH5as8OGnQzwJyA',
+    'UCyU5wkjgQYGRB0hIHMwm2Sg',
+    'UCxX9wt5FWQUAAz4UrysqK9A',
+    'UC5Wi_NYysX-LfcqT3Hq9Faw',
+    'UCVyRiMvfUNMA1UPlDPzG5Ow'
+  ]
+}
 
-//users: thecodingtrain, traversymedia, thenetninjauk, PizzaPokerGuy, CodeNewbies, freeCodeCamp, ossia
+//users: thecodingtrain, traversymedia, thenetninjauk, PizzaPokerGuy, CodeNewbies, freeCodeCamp, ossia, me
 
 //block: fccbarkinet, _30days30sites, this bot
 
