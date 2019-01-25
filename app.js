6c068f5becb1b0c324a9856e6ce542c5d9106dde
Nicole <nicole.sattler@live.com>
2019-01-25 12:37:09 -0600

    Update stream to not limit posts by followed users

--- app.js
+++ app.js
@@ -10,20 +10,24 @@ const Bot = require('./config/bot'),
 setIntervalImediate(tweetVid, 1000 * 60 * 60 * 2)
 
 //Tweet stream config to follow users and hashtags
-var stream = Bot.stream('statuses/filter', { track: hashtags, follow: users })
+const Stream = Bot.stream('statuses/filter', {
+  track: hashtags,
+  follow: users
+})
 
 //Listen for tweet events from tracked hashtags and users
-stream.on('tweet', function(tweet) {
-  //Retweet if tweet is not a reply and not that one annoying user
-  if (!isReply(tweet) && block.indexOf(tweet.user.id_str) == -1) {
+Stream.on('tweet', function(tweet) {
+  console.log('INCOMING: ', tweet.text)
+  if (!isReply(tweet) && !block.includes(tweet.user.id_str)) {
     let extended = tweet.extended_tweet
-
     if (!tweet.truncated && tweet.entities.hashtags.length <= 4) {
-      console.log('LENGTH: ', tweet.entities.hashtags.length)
-      rand(10) === 5 && retweet(tweet.id_str)
+      users.includes(tweet.user.id_str)
+        ? retweet(tweet.id_str)
+        : rand(10) === 5 && retweet(tweet.id_str)
     } else if (extended != null && extended.entities.hashtags.length <= 4) {
-      console.log('LENGTH EXTENDED: ', extended.entities.hashtags.length)
-      rand(10) === 5 && retweet(tweet.id_str)
+      users.includes(tweet.user.id_str)
+        ? retweet(tweet.id_str)
+        : rand(10) === 5 && retweet(tweet.id_str)
     }
   }
 })
